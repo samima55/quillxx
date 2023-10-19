37074f0cfdf6bea2172d03a85d837524ade957fc
Samima Hassan <samimahassan55@gmail.com>
2023-10-19 19:57:23 +0200

    the ui shadcn and button is added , global variable black and white is added , utils function cn is added , styles for get started button added

--- quillxx/src/lib/utils.tsx
+++
@@ -1,9 +0,0 @@
-import { clsx } from "clsx";
-import { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-
-export function cn(...inputs: ClassValue[]){
-    //merge content inside the classname for components 
-    return twMerge(clsx(inputs));
-}
