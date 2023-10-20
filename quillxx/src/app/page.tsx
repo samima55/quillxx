57590e677ef3a2901d2d88b1b121955d57339e6c
Samima Hassan <samimahassan55@gmail.com>
2023-10-20 15:44:03 +0200

    dashboard picture is added

--- quillxx/src/app/page.tsx
+++ quillxx/src/app/page.tsx
@@ -42,7 +42,24 @@ export default function Home() {
               className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
             />
           </div>
+          
 
+          <div>
+            <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+              <div className='mt-16 flow-root sm:mt-24'>
+                <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                  <Image
+                    src=''
+                    alt='product preview'
+                    width={1364}
+                    height={866}
+                    quality={100}
+                    className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
