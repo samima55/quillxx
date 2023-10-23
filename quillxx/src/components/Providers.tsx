e4f403fc64c93918641ecd8518c8a355c4d32dcb
Samima Hassan <samimahassan55@gmail.com>
2023-10-23 12:15:18 +0200

    proivers localhost api

---
+++ quillxx/src/components/Providers.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useState } from "react";
+import {QueryClient} from "@tanstack/react-query"
+import { trpc } from "@/app/_trpc/client";
+import { httpBatchLink } from "@trpc/client";
+ 
+const Providers = () => {
+   const [queryClient] = useState(() => new QueryClient());
+   const [trpcClient] = useState(()=> trpc.createClient({
+      links:[
+         httpBatchLink({
+            url:'http://localhost:3000/api/trpc',
+         })
+      ]
+   }))
+}
+
+export default Providers;
